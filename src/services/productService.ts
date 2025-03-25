074c6291be5489c09277d3437a867b2b80800312
Aarón Más <aaronmasm85@gmail.com>
2025-03-24 19:38:32 -0500

    first commit

---
+++ src/services/productService.ts
@@ -0,0 +1,102 @@
+import { safeParse, pipe, string, transform, number, parse } from "valibot";
+import axios from "axios";
+import {
+  DraftProductSchema,
+  ProductsSchema,
+  Product,
+  ProductSchema,
+} from "../types";
+import { toBoolean } from "../utils";
+
+type ProductDataType = {
+  [p: string]: FormDataEntryValue;
+};
+
+export async function addProduct(data: ProductDataType) {
+  try {
+    const result = safeParse(DraftProductSchema, {
+      name: data.name,
+      price: +data.price,
+    });
+
+    if (result.success) {
+      const url = `${import.meta.env.VITE_API_URL}/api/products`;
+      await axios.post(url, {
+        name: result.output.name,
+        price: result.output.price,
+      });
+    } else {
+      throw new Error("Datos no válidos");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function getProducts() {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products`;
+    const { data } = await axios(url);
+    const result = safeParse(ProductsSchema, data.data);
+    if (result.success) {
+      return result.output;
+    } else {
+      throw new Error("Hubo un error...");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function getProductById(id: Product["id"]) {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+    const { data } = await axios(url);
+    const result = safeParse(ProductSchema, data.data);
+    if (result.success) {
+      return result.output;
+    } else {
+      throw new Error("Hubo un error...");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function editProduct(data: ProductDataType, id: Product["id"]) {
+  try {
+    const NumberSchema = pipe(string(), transform(Number), number());
+
+    const result = safeParse(ProductSchema, {
+      id,
+      name: data.name,
+      price: parse(NumberSchema, data.price),
+      availability: toBoolean(data.availability.toString()),
+    });
+
+    if (result.success) {
+      const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+      await axios.put(url, result.output);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function deleteProduct(id: Product["id"]) {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+    await axios.delete(url);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function updateProductAvailability(id: Product["id"]) {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+    await axios.patch(url);
+  } catch (e) {
+    console.log(e);
+  }
+}
